f1832bdf8f9f6dffeae25cfef07dcbf9f65d24f4
Batool-AA <ba07612@st.habib.edu.pk>
2025-01-15 09:03:34 +0500

    deployment changes reverted

--- app.py
+++ app.py
@@ -158,8 +158,8 @@ def get_job():
         print(f"Error during file upload processing: {e}")
         return jsonify({'error': str(e)}), 500
     
-# if __name__ == '__main__':
-#     webbrowser.open('http://127.0.0.1:5000/')
-#     app.run(debug=True)
+if __name__ == '__main__':
+    webbrowser.open('http://127.0.0.1:5000/')
+    app.run(debug=True)
 
-handler = app
+# handler = app
